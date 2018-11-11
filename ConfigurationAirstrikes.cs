b5d586b91dcd373966a61136282fa1dbc52256db
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2018-11-11 20:28:35 +0000

    Add files via upload

---
+++ ConfigurationAirstrikes.cs
@@ -0,0 +1,34 @@
+﻿using Rocket.API;
+
+namespace Airstrikes
+{
+    public class ConfigurationAirstrikes : IRocketPluginConfiguration
+    {
+        public float MaxAirstrikeDistance;
+        public float MinAirstrikeDistance;
+        public int StrikeCount;
+        public float StartDelay;
+        public float StrikeDelayMin;
+        public float StrikeDelayMax;
+        public float DamageIntensity;
+        public float DamageRadius;
+        public bool LogAirstrikes;
+        public bool BroadcastAirstrikes;
+        
+
+        public void LoadDefaults()
+        {
+            MaxAirstrikeDistance = 250;
+            MinAirstrikeDistance = 50;
+            StrikeCount = 35;
+            StartDelay = 5;
+            StrikeDelayMin = 0.25f;
+            StrikeDelayMax = 0.75f;
+            DamageIntensity = 15;
+            DamageRadius = 20;
+            LogAirstrikes = false;
+            BroadcastAirstrikes = true;
+        }
+
+    }
+}
