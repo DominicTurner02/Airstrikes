f44bfa272ab04b2c75bb698c298a0113d11dd7bb
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2018-11-19 17:02:31 +0000

    Update ConfigurationAirstrikes.cs

--- ConfigurationAirstrikes.cs
+++ ConfigurationAirstrikes.cs
@@ -8,7 +8,6 @@ namespace Airstrikes
         public float MaxAirstrikeDistance;
         public float MinAirstrikeDistance;
         public int StrikeCount;
-        public int SetStrikeCount;
         public float StartDelay;
         public float StrikeDelayMin;
         public float StrikeDelayMax;
@@ -27,7 +26,6 @@ namespace Airstrikes
             MaxAirstrikeDistance = 250;
             MinAirstrikeDistance = 50;
             StrikeCount = 35;
-            SetStrikeCount = 10;
             StartDelay = 5;
             StrikeDelayMin = 0.25f;
             StrikeDelayMax = 0.75f;
